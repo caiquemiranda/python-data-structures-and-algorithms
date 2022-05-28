20e43c88ade23a9d93269c582691820db1e788ad
caiquemiranda <caiquemirandas@gmail.com>
2022-05-28 19:32:17 -0300

    busca-binaria

--- binary_search.py
+++ binary_search.py
@@ -18,21 +18,31 @@ encontrado = False
 # entrada de valor pelo usuário
 numero = int(input("Digite un numero: "))
 
+# iteração no vetor condição enquanto não for encontrado
+# e tamanho do ponteiro for menor ou igual não para
 while not(encontrado) and ponteiro <= tamanhovetor:	
-	metade = int((ponteiro+tamanhovetor) / 2)
 	
+	# metade do vetor
+	metade = int(( ponteiro + tamanhovetor ) / 2)
+	
+	# se o numero estiver na metade(indice-metade) 
+	# então encontrado
 	if numero == vector[metade]:		
 		encontrado = True
 	
+	# se o numero for menor
 	elif numero < vector[metade]:		
 		tamanhovetor = metade - 1
 	
+	# qualquer outra condição
 	else:	
 		ponteiro = metade + 1
 
+# se econtrar o valor 
 if(encontrado):
 	print("posição do valor ", str(metade+1))
 	print(vector)
 
+# se não encontrar o valor
 else:
 	print("valor não econtrado")
