181fd6e23bb4fdc8e7cd878351ff84ce21b23ba6
caiquemiranda <caiquemirandas@gmail.com>
2022-05-28 19:24:30 -0300

    busca-binária

--- binary_search.py
+++ binary_search.py
@@ -1,13 +1,21 @@
-
+# algoritmo de busca binária
 
 # vetor utilizado para buscar
 vector = [8, 3, 5, 9, 10, 22, 45, 500, 455, 900, 4253]
 
+# ordenação do vetor
 vector.sort()
+
+# variavel ponteiro inicializada
 ponteiro = 0
+
+# variavel com tamanho do vetor
 tamanhovetor = len(vector)
+
+# condição de busca do numero
 encontrado = False
 
+# entrada de valor pelo usuário
 numero = int(input("Digite un numero: "))
 
 while not(encontrado) and ponteiro <= tamanhovetor:	
