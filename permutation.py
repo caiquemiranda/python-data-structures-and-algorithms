996faf089e7b42b4247f0d92eb7a295febb8ee42
caiquemiranda <caiquemirandas@gmail.com>
2022-05-28 19:35:32 -0300

    permutation

--- permutation.py
+++ permutation.py
@@ -1,12 +1,19 @@
 # algortimo de permutação
 
 
+# criação da função
 def permutation(data, n):
+    
+    # se quantidade for 1 retorna o proprio valor
     if n == 1:
+
+        # imprime o unico valor
         print(data)
         return
     
+    # se tiver mais de um valor, vai iterar pela quantidade de valores 
     for i in range(n):
+
         permutation(data, n-1)
 
         if n % 2 == 0:
@@ -15,5 +22,5 @@ def permutation(data, n):
         else:
             data[0], data[n-1] = data[n-1], data[0]
 
-data = [1, 2]
+data = [1, 2, 4, 6]
 permutation(data, len(data))
